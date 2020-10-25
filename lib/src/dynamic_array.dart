7f6d86d407ae41a8b2fd14d2a54025f11b69e60a
alex.tifox <tilnyi.olexander@gmail.com>
2020-10-25 22:45:27 +0100

    Initial commit

---
+++ lib/src/dynamic_array.dart
@@ -0,0 +1,41 @@
+class DynamicArray<T> {
+  DynamicArray() {
+
+  }
+  List<T> dynamicArray;
+  int currentSize;
+  int maxSize = 1;
+  final double expandCoefficient = 1.5;
+
+  void add(T data) {
+
+  }
+
+  T get(int index) {
+
+  }
+
+  void set(int index, T data) {
+
+  }
+
+  void removeAll() {
+
+  }
+
+  void arrayToString(String Function(T) objectToStringFunction) {
+
+  }
+
+  void sort(bool Function(T, T) comparator) {
+
+  }
+
+  void _arrayInit() {
+
+  }
+
+  void _expandArray() {
+
+  }
+}
---
+++ lib/src/main.dart
@@ -0,0 +1,8 @@
+class Main {
+
+  static void main() {
+    print("object");
+  }
+}
+
+void main() => Main.main();

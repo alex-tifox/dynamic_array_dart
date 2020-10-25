fec437e2de43bcae3962bc4ce484906d6ac4088f
alex.tifox <tilnyi.olexander@gmail.com>
2020-10-25 23:49:41 +0100

    Main experiment

--- lib/src/main.dart
+++ lib/src/main.dart
@@ -1,7 +1,43 @@
+import 'dart:math';
+
+import 'dynamic_array.dart';
+import 'primitive.dart';
+
 class Main {
 
   static void main() {
-    print("object");
+    DynamicArray<Primitive> dynamicArray = new DynamicArray();
+    final int order = 5;
+    final int numberOfElements = pow(10, order);
+
+    int t1 = DateTime.now().millisecondsSinceEpoch;
+    double maxTimePerElement = 0.0, worstTimePerElement;
+    int worstIndex;
+
+    for (int i = 0; i < numberOfElements; i++) {
+      int t1_element = DateTime.now().millisecondsSinceEpoch;
+
+      dynamicArray.add(Primitive());
+      int t2_element = DateTime.now().millisecondsSinceEpoch;
+      double timePerElement = (t2_element - t1_element) / 1000;
+
+      if (timePerElement > maxTimePerElement) {
+        maxTimePerElement = timePerElement;
+        worstTimePerElement = timePerElement;
+        worstIndex = i;
+        print('Index is: $i. Bad time: $timePerElement seconds');
+      }
+    }
+
+    dynamicArray.sort((left, right) => left.someNumber > right.someNumber);
+
+    int t2 = DateTime.now().millisecondsSinceEpoch;
+    double time = (t2 - t1) / 1000;
+    print('Worst index: $worstIndex. Bad time: $worstTimePerElement seconds');
+    print('All time is: $time seconds');
+
+    dynamicArray.arrayToString((number) => number.toString(), 20);
+    dynamicArray.removeAll();
   }
 }
 
--- lib/src/primitive.dart
+++ lib/src/primitive.dart
@@ -1,3 +1,13 @@
+import 'dart:math';
+
 class Primitive {
 
+  int someNumber;
+
+  Primitive() : this.someNumber = Random.secure().nextInt(1000000);
+
+  @override
+  String toString() {
+    return 'Primitive { someNumber: $someNumber }';
+  }
 }
